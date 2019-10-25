d0e5b572e961b8b62a7199b3d88c08e3eddcabc9
Luiz Santos <luizcsbh@gmail.com>
2019-10-25 02:09:44 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Routes from "./routes";
 import "./styles.css";
 
 import Header from './components/Header';
@@ -7,7 +7,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App">
       <Header />
-      <Main />
+      <Routes />
   </div>
 );
 export default App;
